f88cd682ab3763e6fb484823540beda9ceea93d0
Matthew Todd Hayton <todd.hayton@gmail.com>
2018-07-18 10:37:19 -0400

    Distinguish between results table being loaded for the first time (in which case we can simply wait until the selector appears) vs when the data is updated by a new search (in which case we need to wait for the existing table data to become stale).
    Create a waitUntilStale() function to indicate when an element has detached
    from the DOM.

--- rid/rid_scraper.js
+++ rid/rid_scraper.js
@@ -1,6 +1,16 @@
 const puppeteer = require('puppeteer');
 const url = 'https://myaccount.rid.org/Public/Search/Member.aspx';
 
+/*
+ * Wait until elem becomes detached from DOM
+ */
+async function waitUntilStale(page, elem) {
+    await page.waitForFunction(
+        e => !e.ownerDocument.contains(e),
+        { polling: 'raf' }, elem
+    );
+}
+
 async function getSelectOptions(page, selector) {
     const options = await page.evaluate(optionSelector => {
         return Array.from(document.querySelectorAll(optionSelector))
@@ -42,10 +52,7 @@ async function setMaxPageSize(page) {
      * So we wait until the old member table gets detached from the DOM as the signal
      * that the new table has been loaded
      */
-    await page.waitForFunction(
-        e => !e.ownerDocument.contains(e),
-        { polling: 'raf' }, memberTable
-    );
+    await waitUntilStale(page, memberTable);
 }
 
 /*------------------------------------------------------------------------------
@@ -151,8 +158,8 @@ async function scrapeAllPages(page) {
 }
 
 async function run() {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
-    //const browser = await puppeteer.launch({ headless: true, args: [ '--start-fullscreen' ] });
+    //const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+    const browser = await puppeteer.launch({ headless: true, args: [ '--start-fullscreen' ] });
     const page = await browser.newPage();
 
     page.on('console', msg => console.log('PAGE LOG:', msg.text()));
@@ -161,20 +168,39 @@ async function run() {
 
     let states = await getStates(page);
     
-    for (state of states) {
-        console.log(`Scraping data for ${state.name}`);
+    for (const [ i, state ] of states.entries()) {
+        console.log(`[${i+1}/${states.length}] Scraping data for ${state.name}`);
         
         await page.select('#FormContentPlaceHolder_Panel_stateDropDownList', state.value);
         await page.select('#FormContentPlaceHolder_Panel_freelanceDropDownList', '1');
 
+        /*
+         * The first time we run a search we can wait for the table to appear to determine
+         * once the search has loaded the results. But with subsequent searches we need to
+         * determine when the table contents have been updated. To do that we fetch a ref
+         * to the table here and then wait for it to become stale (detached) as an indication
+         * that the new table data has loaded.
+         */
+        let resultsTable = await page.$('table#FormContentPlaceHolder_Panel_resultsGrid');
+        
         await page.click('#FormContentPlaceHolder_Panel_searchButtonStrip_searchButton');
-        await page.waitForSelector('#FormContentPlaceHolder_Panel_resultsGrid');
 
-        await setMaxPageSize(page);
+        if (resultsTable) {
+            await waitUntilStale(page, resultsTable);
+        } else {
+            await page.waitForSelector('#FormContentPlaceHolder_Panel_resultsGrid');
+        }
+
+        if (i === 0) {
+            await setMaxPageSize(page);
+        }
 
         let data = await scrapeAllPages(page);
         console.log(`Got ${data.length} records in all`);
-        break;
+
+        if (i >= 2) {
+            break;
+        }
     }
 
     browser.close();
